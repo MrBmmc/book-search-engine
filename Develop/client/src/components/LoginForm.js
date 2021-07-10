071c6ae44abbde3534e3022d9ec7499f4c231ac4
Brian McMullen <clanmcmullen2012@gmail.com>
2021-07-10 11:02:04 -0400

    updating src files

--- Develop/client/src/components/LoginForm.js
+++ Develop/client/src/components/LoginForm.js
@@ -1,15 +1,24 @@
-// see SignupForm.js for comments
+// see SignupForm.js for comments GIVEN
 import React, { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
 
-import { loginUser } from '../utils/API';
+// import { loginUser } from '../utils/API';
 import Auth from '../utils/auth';
 
+import { useMutation } from '@apollo/client';
+import { LOGIN_USER } from '../utils/mutations';
+
+
+//GIVEN
 const LoginForm = () => {
   const [userFormData, setUserFormData] = useState({ email: '', password: '' });
   const [validated] = useState(false);
   const [showAlert, setShowAlert] = useState(false);
 
+  //Use Mutation
+  const [loginUser, {error, data}] = useMutation(LOGIN_USER);
+  
+  //GIVEN
   const handleInputChange = (event) => {
     const { name, value } = event.target;
     setUserFormData({ ...userFormData, [name]: value });
@@ -26,19 +35,26 @@ const LoginForm = () => {
     }
 
     try {
-      const response = await loginUser(userFormData);
+      const { data } = await loginUser({ variables: {...userFormData}});
 
-      if (!response.ok) {
-        throw new Error('something went wrong!');
-      }
-
-      const { token, user } = await response.json();
-      console.log(user);
-      Auth.login(token);
+      Auth.login(data.login.token);
+       
     } catch (err) {
       console.error(err);
       setShowAlert(true);
     }
+
+    //   if (!response.ok) {
+    //     throw new Error('something went wrong!');
+    //   }
+
+    //   const { token, user } = await response.json();
+    //   console.log(user);
+    //   Auth.login(token);
+    // } catch (err) {
+    //   console.error(err);
+    //   setShowAlert(true);
+    // }
 
     setUserFormData({
       username: '',
---
+++ Develop/client/src/utils/mutations.js
@@ -0,0 +1,47 @@
+import { gql } from '@apollo/client';
+
+export const LOGIN_USER = gql`
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+        name
+      }
+    }
+  }
+`;
+
+export const ADD_USER = gql`
+    mutation createUser($username: String!, $email: String!, $password: String!){
+        createUser(username: $username, email: $email, password: $password){
+            token
+            user {
+                _id
+                username
+            }
+        }
+    }
+`
+
+export const SAVE_BOOK = gql`
+    mutation saveBook($authors: [String]!, $description: String!, $title: String!, $bookId: String!, $image: String, $link: String) {
+        saveBook(authors: $authors, description: $description, title: $title, bookId: $bookId, image: $iamge, link: $link){
+            _id
+            username
+        }
+    }
+`
+
+export const REMOVE_BOOK = gql`
+    mutation removeBook($bookId: String!) {
+        removeBook(bookId: $bookId) {
+            username
+            savedBooks {
+                bookId
+                authors
+                description
+            }
+        }
+    }
+`;
